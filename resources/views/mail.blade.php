f1d94045eb233966efe7c04758d8a20a073d1656
VorapratR <vorapratrattana@gmail.com>
2019-02-26 19:40:08 +0700

    c

--- resources/views/mail.blade.php
+++ resources/views/mail.blade.php
@@ -1,2 +1,24 @@
-<h1>Hi, {{ $email }}</h1>
-l<p>{!! $content !!}.</p>
+<style>
+    h2{
+        text-align: center;
+    }
+    .content{
+        border: 1px solid #008299;
+        border-radius: 5px;
+        padding: 15px;
+    }
+    .signature{
+        text-align: right;
+        padding-top: 30px;
+    }
+</style>
+<h2>สวัสดี {{ $email }}</h2>
+<p><strong>แจ้งเรื่อง</strong> {{ $subject }}</p>
+<div class="content">
+    <p>{!! $content !!}</p>
+
+    <div class="signature">
+        <p>ด้วยความนับถือ</p>
+        <p>5910110288</p>
+    </div>
+</div>
